f74dbbf2acf39a80771af90d8ff58b4be3a3ff7b
DONGHOON IM <dongttang@gmail.com>
2019-11-11 11:10:31 +0900

    add middleware for using res.locals

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import { localsMiddleware } from "./middlewares";
 import routes from "./routes";
 import globalRouter from "./routes/globalRouter";
 import userRouter from "./routes/userRouter";
@@ -10,12 +11,13 @@ import videoRouter from "./routes/videoRouter";
 
 const app = express();
 
+app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.text({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
+app.use(localsMiddleware);
 
 app.use(routes.root, globalRouter);
 app.use(routes.user, userRouter);
